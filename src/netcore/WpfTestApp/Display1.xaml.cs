eafe6278a6f8cc356a5504d60921553f280b6d10
Matthes <mk.public@outlook.com>
2020-05-22 12:36:48 +0200

    First Version

---
+++ src/netcore/WpfTestApp/Display1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Interaction logic for Display1.xaml
+    /// </summary>
+    public partial class Display1 : UserControl
+    {
+        public Display1()
+        {
+            InitializeComponent();
+        }
+    }
+}
